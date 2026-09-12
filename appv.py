6f4605401826d30ad6e9fc7015e11acd034ae3ec
abdeljalillariam-hub <abdeljalillariam@gmail.com>
2026-09-12 15:15:18 +0100

    Add streamlit-image-coordinates import

--- appv.py
+++ appv.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import streamlit as st
 from PIL import Image, ImageDraw
+from streamlit-image-coordinates import streamlit-image-coordinates
 st.set_page_config(page_title="AutoMRI V3", layout="wide")
 st.title("AutoMRI V3 – MRI Parameter Simulator")
 st.caption("Interactive 3-plane localizer + educational MRI parameter simulator.")
